d5773f553c850dde68f3ae76d236f7084a5d8382
0xRigel <orion@sqds.io>
2025-03-02 15:03:46 +0100

    feat(sdk): finalize terminology refactor

---
+++ sdk/smart-account/src/rpc/setArchivalAuthorityAsAuthority.ts
@@ -0,0 +1,52 @@
+import {
+  Connection,
+  PublicKey,
+  SendOptions,
+  Signer,
+  TransactionSignature,
+} from "@solana/web3.js";
+import * as transactions from "../transactions";
+import { translateAndThrowAnchorError } from "../errors";
+
+/** Set the smart account `archival_authority`. */
+export async function setArchivalAuthorityAsAuthority({
+  connection,
+  feePayer,
+  settingsPda,
+  settingsAuthority,
+  newArchivalAuthority,
+  memo,
+  signers,
+  sendOptions,
+  programId,
+}: {
+  connection: Connection;
+  feePayer: Signer;
+  settingsPda: PublicKey;
+  settingsAuthority: PublicKey;
+  newArchivalAuthority: PublicKey | null;
+  memo?: string;
+  signers?: Signer[];
+  sendOptions?: SendOptions;
+  programId?: PublicKey;
+}): Promise<TransactionSignature> {
+  const blockhash = (await connection.getLatestBlockhash()).blockhash;
+
+  const tx = transactions.setArchivalAuthorityAsAuthority({
+    blockhash,
+    feePayer: feePayer.publicKey,
+    settingsPda,
+    settingsAuthority,
+    newArchivalAuthority,
+    memo,
+    programId,
+  });
+
+  tx.sign([feePayer, ...(signers ?? [])]);
+
+  try {
+    return await connection.sendTransaction(tx, sendOptions);
+  } catch (err) {
+    translateAndThrowAnchorError(err);
+  }
+}
