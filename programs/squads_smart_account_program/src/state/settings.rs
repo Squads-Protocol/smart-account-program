75c2cc94386efb0c9d4dc8fccb34f44d9599e4f9
0xLeo-sqds <leo-gh@sqds.io>
2025-12-17 10:54:36 +0100

    Missing Update to time_constraints.start in SettingsAction::PolicyUpdate fixed

--- programs/squads_smart_account_program/src/state/settings.rs
+++ programs/squads_smart_account_program/src/state/settings.rs
@@ -612,8 +612,19 @@ impl Settings {
                     ) => PolicyState::ProgramInteraction(creation_payload.to_policy_state()?),
                     (
                         PolicyState::SpendingLimit(_),
-                        PolicyCreationPayload::SpendingLimit(creation_payload),
-                    ) => PolicyState::SpendingLimit(creation_payload.to_policy_state()?),
+                        PolicyCreationPayload::SpendingLimit(mut creation_payload),
+                    ) => {
+                        // If accumulate unused is true, and the policy has a
+                        // start date in the past, set it to the current
+                        // timestamp to avoid unintended accumulated usage
+                        let current_timestamp = Clock::get()?.unix_timestamp;
+                        if creation_payload.time_constraints.accumulate_unused
+                            && creation_payload.time_constraints.start < current_timestamp
+                        {
+                            creation_payload.time_constraints.start = current_timestamp;
+                        }
+                        PolicyState::SpendingLimit(creation_payload.to_policy_state()?)
+                    }
                     (
                         PolicyState::SettingsChange(_),
                         PolicyCreationPayload::SettingsChange(creation_payload),
